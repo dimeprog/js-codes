da3c771598960e495224d1f37631e7c6da4bb671
dimeprog <dimejiawoyefa5@gmail.com>
2022-07-07 05:44:03 +0100

    checking increament function in Js

---
+++ index.js
@@ -0,0 +1,13 @@
+let count = 0;
+let half=0;
+
+const increament = function  ()  {
+    console.log('working');
+     half= count++;
+};
+const decreamennt = () => count--;
+console.log('hello world');
+console.log(count);
+console.log(half);
+
+
